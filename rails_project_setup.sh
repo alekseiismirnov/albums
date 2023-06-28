d61ddae427caf2e51a55229c977657089051fed0
AS <alekseii.sm@gmail.com>
2023-06-29 01:29:26 +0300

    add newly generated rails project

--- rails_project_setup.sh
+++ rails_project_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "*** Rails Project Setup ***"
-[ -f /app/albums/bin/rails ] || rails new /app/albums -d postgresql -T -s -c bootstrap 
+[ -f /app/albums/bin/rails ] || rails new /app/albums -d postgresql -T -s -c bootstrap --skip-git
 /app/albums/bin/bundle install
 /app/albums/bin/rails db:prepare
 rm -fr /app/albums/tmp/pids/server.pid
