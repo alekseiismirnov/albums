a654556353e19eb6a2cbfb87e67900483e428ba4
Alekseii Sm <alekseii.sm@gmail.com>
2023-09-08 17:50:27 +0300

    remove demo-code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "popper"
 import "bootstrap"
-
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
-let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
-  return new bootstrap.Popover(popoverTriggerEl)  
-})  
