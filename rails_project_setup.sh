c2ccb745e5c8c258ed494e41b998f76a48d2fcea
Alekseii Sm <alekseii.sm@gmail.com>
2023-09-23 17:52:27 +0300

    fix rails_project_setup

--- rails_project_setup.sh
+++ rails_project_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo "*** Rails Project Setup ***"
-[ -f /app/albums/bin/rails ] || rails new /app/albums -d postgresql -T -s -c bootstrap --skip-git
+ ! [ -x /app/albums/bin/rails ] && { rails new /app/albums -d postgresql -T -s -c bootstrap --skip-git
 /app/albums/bin/bundle install
 /app/albums/bin/rails db:prepare
-#FIXME /app/albums/bin/bundle exec spring binstub rspec
-#FIXME /app/albums/bin/rails css:install:bootstrap 
+/app/albums/bin/bundle exec spring binstub rspec
+/app/albums/bin/rails css:install:bootstrap 
 /app/albums/bin/importmap pin glightbox
+}
 
 rm -fr /app/albums/tmp/pids/server.pid
 /app/albums/bin/rails s -b 0.0.0.0
