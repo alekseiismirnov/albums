43d74dba9891c0cfddf5275719de26ed79aae253
Alekseii Sm <alekseii.sm@gmail.com>
2023-12-02 15:55:11 +0200

    makes faker data repetetive. presumably

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,10 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.before(:each) do 
+    Faker::Config.random = Random.new(1101)
+    Faker::UniqueGenerator.clear
+  end  
 
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
