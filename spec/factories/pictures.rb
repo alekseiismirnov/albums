c8970f256db0a2661cd96dd9a5a9e60f7db02d48
Alekseii Sm <alekseii.sm@gmail.com>
2023-12-09 15:47:50 +0200

    fix hardcoded path using for factory bot stub images

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :picture do 
     # belongs_to
     user
-    file { Rack::Test::UploadedFile.new('spec/fixtures/stub.jpg', 'image/jpeg')}
+    file { Rack::Test::UploadedFile.new(file_fixture('stub.jpg'), 'image/jpeg')}
     after(:create) do |picture, _|
       create_list(:tag, 5, pictures: [picture])
     end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -6,3 +6,17 @@ RSpec.configure do |config|
     Faker::UniqueGenerator.clear
   end  
 end
+
+module FactoryBotFileFixtureHelpers
+  extend ActiveSupport::Concern
+
+  include ActiveSupport::Testing::FileFixtures
+  include ActionDispatch::TestProcess::FixtureFile
+
+  included do
+    self.file_fixture_path = RSpec.configuration.file_fixture_path
+  end
+end
+
+FactoryBot::Evaluator.include FactoryBotFileFixtureHelpers
+FactoryBot::SyntaxRunner.include FactoryBotFileFixtureHelpers
